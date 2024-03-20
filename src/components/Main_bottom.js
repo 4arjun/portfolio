2c63614d6e2868195f56f4db177f7f084db50ef8
ARJUN AJITH <arjun@ARJUNs-MacBook-Pro.local>
2024-03-20 13:15:13 +0530

    setup fix

---
+++ src/components/Main_bottom.js
@@ -0,0 +1,10 @@
+import './Main_bottom.css'
+
+const Main_bottom = () => {
+    return (
+        <div className='bottom-container'>
+            <p>MEC'27</p>
+        </div>
+    )
+}
+export default Main_bottom
