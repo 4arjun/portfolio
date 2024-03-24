5f1b1feae10e378f2fa09852e98828816c8e28ce
4arjun <arjunajith440@gmail.com>
2024-03-24 17:20:28 +0530

    home-animations

--- src/components/Main_bottom.js
+++ src/components/Main_bottom.js
@@ -1,8 +1,17 @@
+import { useEffect, useState } from 'react'
 import './Main_bottom.css'
 
 const Main_bottom = () => {
+    const[isVisible,setIsVisible] = useState(false);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(true);
+        }, 920);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className='bottom-container'>
+        <div className={`bottom-container ${isVisible ? 'visible' : ''}`}>
             <p>MEC'27</p>
     
         </div>
