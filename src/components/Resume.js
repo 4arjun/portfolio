d9dd2ef1802b620f032873a5477c1f78310c6b39
ARJUN AJITH <arjun@ARJUNs-MacBook-Pro.local>
2024-03-24 17:20:28 +0530

    home-animations

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './resume.css';
-import resume from '../assets/ML_CV1_ARJUN_AJITH.pdf'
-
+import resume from '../assets/ML_CV1_ARJUN_AJITH (1).pdf'
 const Resume = () => {
     return (
         <div className='resume-container'>
