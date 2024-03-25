03eaf90fc83a651af6024f9c66ac456a0c9081f4
4arjun <arjunajith440@gmail.com>
2024-03-25 20:39:39 +0530

    delay function with animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,25 @@
 import './Nav.css';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
 const Nav = () => {
-    
+    const handleClick = (e) => {
+        e.preventDefault();
+        setTimeout(() => {
+            window.location.href = e.target.href;
+        }, 500);
+    };
+
     return (
         <div className='nav-container'>
             <div className='toggle-bar'>
                 <p>button</p>
             </div>
             <div className='menu-list'>
-                <Link to="/about" className='list-items'>About</Link>
-                <Link to="/resume"className='list-items'>Resume</Link>
-                <Link to="/project" className='list-items'>Projects</Link>
-                <Link to="https://github.com/4arjun" className='list-items'>Github</Link>
+                <Link to="/about" className='list-items' onClick={handleClick}>About</Link>
+                <Link to="/resume" className='list-items' onClick={handleClick}>Resume</Link>
+                <Link to="/project" className='list-items' onClick={handleClick}>Projects</Link>
+                <Link to="https://github.com/4arjun" className='list-items' onClick={handleClick}>Github</Link>
+                
             </div>
         </div>
     );
