bc9a98e47ddfd48cbf63b8cdde8147b69b9a453b
4arjun <arjunajith440@gmail.com>
2024-03-25 21:13:59 +0530

    fix 3

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './Nav.css';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
-    const handleClick = (e) => {
+    const handleClick = (e, link) => {
         e.preventDefault();
         setTimeout(() => {
-            window.location.href = e.target.href;
+            window.location.href = link;
         }, 300);
     };
 
@@ -15,11 +16,10 @@ const Nav = () => {
                 <p>button</p>
             </div>
             <div className='menu-list'>
-                <Link to="/about" className='list-items' onClick={handleClick}>About</Link>
-                <Link to="/resume" className='list-items' onClick={handleClick}>Resume</Link>
-                <Link to="/project" className='list-items' onClick={handleClick}>Projects</Link>
-                <Link to="https://github.com/4arjun" className='list-items' onClick={handleClick}>Github</Link>
-                
+                <Link to="/about" className='list-items' onClick={(e) => handleClick(e, "/about")}>About</Link>
+                <Link to="/resume" className='list-items' onClick={(e) => handleClick(e, "/resume")}>Resume</Link>
+                <Link to="/project" className='list-items' onClick={(e) => handleClick(e, "/project")}>Projects</Link>
+                <a href="https://github.com/4arjun" className='list-items' onClick={(e) => handleClick(e, "https://github.com/4arjun")}>Github</a>
             </div>
         </div>
     );
