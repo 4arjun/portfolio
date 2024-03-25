4dbb12bc66b1f93fb1441f10e32e174ad9e56a41
ARJUN AJITH <arjun@ARJUNs-MacBook-Pro.local>
2024-03-25 21:08:04 +0530

    error

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,28 @@
 import './Nav.css';
 import { Link } from 'react-router-dom';
 
-const Nav = () => {
+const DelayedLink = ({ to, delay, children }) => {
     const handleClick = (e) => {
         e.preventDefault();
         setTimeout(() => {
-            window.location.href = e.target.href;
-        }, 300);
+            window.location.href = to;
+        }, delay);
     };
 
+    return <a href={to} onClick={handleClick}>{children}</a>;
+};
+
+const Nav = () => {
     return (
         <div className='nav-container'>
             <div className='toggle-bar'>
                 <p>button</p>
             </div>
             <div className='menu-list'>
-                <Link to="/about" className='list-items' onClick={handleClick}>About</Link>
-                <Link to="/resume" className='list-items' onClick={handleClick}>Resume</Link>
-                <Link to="/project" className='list-items' onClick={handleClick}>Projects</Link>
-                <Link to="https://github.com/4arjun" className='list-items' onClick={handleClick}>Github</Link>
-                
+                <DelayedLink to="/about" delay={300}>About</DelayedLink>
+                <DelayedLink to="/resume" delay={300}>Resume</DelayedLink>
+                <DelayedLink to="/project" delay={300}>Projects</DelayedLink>
+                <DelayedLink to="https://github.com/4arjun" delay={300}>Github</DelayedLink>
             </div>
         </div>
     );
