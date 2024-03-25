d7ef1d37b87b38cfa28fb8007be0854a123b6d53
ARJUN AJITH <arjun@ARJUNs-MacBook-Pro.local>
2024-03-25 22:14:36 +0530

    fix 6

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,12 @@ import './Nav.css';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
-    
+    const handleClick = (e, link) => {
+        e.preventDefault();
+        setTimeout(() => {
+            window.location.href = link;
+        }, 300);
+    };
 
     return (
         <div className='nav-container'>
@@ -11,10 +16,10 @@ const Nav = () => {
                 <p>button</p>
             </div>
             <div className='menu-list'>
-                <Link to="/about" className='list-items'>About</Link>
-                <Link to="/resume" className='list-items'>Resume</Link>
-                <Link to="/project" className='list-items'>Projects</Link>
-                <a href="https://github.com/4arjun" className='list-items'>Github</a>
+                <Link to="/about" className='list-items' onClick={(e) => handleClick(e, "/about")}>About</Link>
+                <Link to="/resume" className='list-items' onClick={(e) => handleClick(e, "/resume")}>Resume</Link>
+                <Link to="/project" className='list-items' onClick={(e) => handleClick(e, "/project")}>Projects</Link>
+                <a href="https://github.com/4arjun" className='list-items' onClick={(e) => handleClick(e, "https://github.com/4arjun")}>Github</a>
             </div>
         </div>
     );
